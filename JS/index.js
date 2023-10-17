ceb48eda8030ad6354f3fda44c1088a1768a1101
lucianasoltermann <lucianasoltermann@gmail.com>
2023-10-17 16:04:29 -0300

    Ultimo commit - entrega final

--- JS/index.js
+++ JS/index.js
@@ -1,124 +1,61 @@
-const arrayIngresos = JSON.parse(localStorage.getItem('arrayIngresos')) || [];
-const arrayGastos = JSON.parse(localStorage.getItem('arrayGastos')) || [];
-const arrayDeudas = JSON.parse(localStorage.getItem('arrayDeudas')) || [];
-const arrayInversiones = JSON.parse(localStorage.getItem('arrayInversiones')) || [];
+const createArrayFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key)) || [];
 
+const arrayIngresos = createArrayFromLocalStorage('arrayIngresos');
+const arrayGastos = createArrayFromLocalStorage('arrayGastos');
+const arrayDeudas = createArrayFromLocalStorage('arrayDeudas');
+const arrayInversiones = createArrayFromLocalStorage('arrayInversiones');
+
+const addToLocalStorageAndList = (array, key, input, list) => {
+    const inputValue = parseFloat(input.value);
+    if (!isNaN(inputValue)) {
+        array.push(inputValue);
+        const item = document.createElement("li");
+        item.innerText = inputValue;
+        list.appendChild(item);
+        localStorage.setItem(key, JSON.stringify(array));
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Input vacío o no válido!',
+        });
+    }
+    input.focus();
+    input.value = '';
+};
 
 const agregarForm = document.querySelector("#agregar-form");
 const agregarInput = document.querySelector("#agregar-input");
 const agregar = document.querySelector("#agregar");
-
-agregarForm.addEventListener("submit", agregarItems);
-
-function agregarItems(e) {
+agregarForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    if (agregarInput.value != "" && !isNaN(agregarInput.value)) {
-        arrayIngresos.push(agregarInput.value);
-        let item = document.createElement("li");
-        item.innerText = agregarInput.value;
-        agregar.append(item);
-
-        localStorage.setItem('arrayIngresos', JSON.stringify(arrayIngresos));
-    } else {
-        Swal.fire({
-            icon: 'error', 
-            title: 'Error',
-            text: 'Input vacío o no válido!',
-          })
-    }
-
-    agregarInput.focus();
-    agregarForm.reset();
-}
+    addToLocalStorageAndList(arrayIngresos, 'arrayIngresos', agregarInput, agregar);
+});
 
 const gastosForm = document.querySelector("#gastos-form");
 const gastosInput = document.querySelector("#gastos-input");
 const gastos = document.querySelector("#gastos");
-
-gastosForm.addEventListener("submit", agregarItemsGastos);
-
-function agregarItemsGastos(e) {
+gastosForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    if (gastosInput.value != "" && !isNaN (gastosInput.value)) {
-        arrayGastos.push(gastosInput.value);
-        let item = document.createElement("li");
-        item.innerText = gastosInput.value;
-        gastos.append(item);
-
-    
-        localStorage.setItem('arrayGastos', JSON.stringify(arrayGastos));
-    } else {
-        Swal.fire({
-            icon: 'error', 
-            title: 'Error',
-            text: 'Input vacío o no válido!',
-          })
-    }
-
-    gastosInput.focus();
-    gastosForm.reset();
-}
+    addToLocalStorageAndList(arrayGastos, 'arrayGastos', gastosInput, gastos);
+});
 
 const deudasForm = document.querySelector("#deudas-form");
 const deudasInput = document.querySelector("#deudas-input");
 const deudas = document.querySelector("#deudas");
-
-deudasForm.addEventListener("submit", agregarItemsDeudas);
-
-function agregarItemsDeudas(e) {
+deudasForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    if (deudasInput.value != "" && !isNaN (deudasInput.value)) {
-        arrayDeudas.push(deudasInput.value);
-        let item = document.createElement("li");
-        item.innerText = deudasInput.value;
-        deudas.append(item);
-
-     
-        localStorage.setItem('arrayDeudas', JSON.stringify(arrayDeudas));
-    } else {
-            Swal.fire({
-                icon: 'error', 
-                title: 'Error',
-                text: 'Input vacío o no válido!',
-              })
-        
-    }
-
-    deudasInput.focus();
-    deudasForm.reset();
-}
+    addToLocalStorageAndList(arrayDeudas, 'arrayDeudas', deudasInput, deudas);
+});
 
 const inversionesForm = document.querySelector("#inversiones-form");
 const inversionesInput = document.querySelector("#inversiones-input");
 const inversiones = document.querySelector("#inversiones");
-
-inversionesForm.addEventListener("submit", agregarItemsInvesiones);
-
-function agregarItemsInvesiones(e) {
+inversionesForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    addToLocalStorageAndList(arrayInversiones, 'arrayInversiones', inversionesInput, inversiones);
+});
 
-    if (inversionesInput.value != "" && !isNaN (inversionesInput.value)) {
-        arrayInversiones.push(inversionesInput.value);
-        let item = document.createElement("li");
-        item.innerText = inversionesInput.value;
-        inversiones.append(item);
-
-       
-        localStorage.setItem('arrayInversiones', JSON.stringify(arrayInversiones));
-    } else {
-            Swal.fire({
-                icon: 'error', 
-                title: 'Error',
-                text: 'Input vacío o no válido!',
-              })
-    }
-
-    inversionesInput.focus();
-    inversionesForm.reset();
-}
 
 const btncalcular = document.querySelector("#btnCalcular");
 const resultadosElement = document.getElementById('resultados');
@@ -192,14 +129,14 @@ btntasa.addEventListener ("click", ()=>{
             const rates = data.rates;
             const ul = document.createElement('ul');
         for (const currency in rates) {
-            const rate = rates[currency];
-            const li = document.createElement('li');
-                li.textContent = `${currency}: ${rate}`;
-                ul.appendChild(li);
+        const rate = rates[currency];
+    const li = document.createElement('li');
+                    li.textContent = `${currency}: ${rate}`;
+                    ul.appendChild(li);
                 }
-    document.getElementById('exchangeRates').appendChild(ul);
+                document.getElementById('exchangeRates').appendChild(ul);
             })
-    .catch(error => {
+            .catch(error => {
                 console.error('Error al obtener las tasas de cambio: ' + error);
             });
 })
